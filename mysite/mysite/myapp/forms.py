d20c93fb598df78fb0aaca7b788fe577e8d86abc
IwoZachary <61707966+IwoZachary@users.noreply.github.com>
2021-01-16 22:07:41 +0100

    composite pk and css

---
+++ mysite/mysite/myapp/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import  UserCreationForm
+from django_enumfield.forms.fields import EnumChoiceField
+
+
+from myapp.models import Account, FileMod, Logs, Rate, SharedFile, Opinion
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=60, help_text='Required valid email')
+
+    class Meta:
+        model=Account
+        fields = ('email', 'username', 'password1', 'password2')
+
+class AccountAuthenticationForm(forms.ModelForm):
+
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model=Account
+        fields= ('email', 'password')
+    
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        if not authenticate(email=email, password=password):
+            raise forms.ValidationError("Invalid login")
+
+
+class FileForm(forms.ModelForm):
+    class Meta:
+        model = FileMod
+        fields = ["is_public", "fileF","owner"]
+
+class ShareForm(forms.ModelForm):
+    class Meta:
+        model = SharedFile
+        fields = ['fileS', 'userS']
+
+    
+
+
+
+class OpinionForm(forms.ModelForm):
+    rate = EnumChoiceField(Rate)
+    class Meta:
+        model = Opinion
+        fields = ["fileS", "userS", "rate"]
+
+class LogsForm(forms.ModelForm):
+    class Meta:
+        model= Logs
+        fields = ['userS', 'action']
---
+++ mysite/mysite/myapp/views.py
@@ -0,0 +1,205 @@
+from django.contrib.auth.models import AnonymousUser
+from myapp.models import Account, Action, SharedFile, Logs
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect
+from .forms import FileForm, OpinionForm, ShareForm
+from django.contrib.auth import login, authenticate, logout
+from myapp.forms import RegistrationForm
+from myapp.forms import AccountAuthenticationForm, FileMod, Opinion, LogsForm
+from django.contrib import messages
+from django.http import HttpResponse
+import os
+from django.http import FileResponse
+from django.views.static import serve
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def registration_view(request):
+        context={}
+        if request.POST:
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                email = form.cleaned_data.get('email')
+                raw_password = form.cleaned_data.get('password1')
+                account = authenticate(email=email, password=raw_password)
+                login(request, account)
+                Logs.objects.create(userS = request.user, action = Action.LOGIN)
+                return redirect('home')
+            else:
+                context['registration_form'] = form
+        else:
+            form = RegistrationForm()
+            context['registration_form'] = form
+        return render(request, 'myapp/register.html', context)
+
+def user_home_view(request):
+    context = {}
+    if request.user.is_authenticated:
+        if request.POST:
+            form = FileForm(request.POST, request.FILES)
+            if form.is_valid() and request.FILES != None :
+                obj = form.save(commit=False)
+                obj.owner = request.user
+                obj.save()
+                Logs.objects.create(userS = request.user, action = Action.UPLOAD)
+                messages.success(request, 'file added')
+            else:
+                form = FileForm()
+                context['file_form'] = form
+        else:
+            form = FileForm()
+            context['file_form'] = form
+        return render( request, 'myapp/user_home.html', context)
+    else:
+        return redirect('home')
+
+def logout_view(request):
+    Logs.objects.create(userS = request.user, action = Action.LOGOUT)
+    logout(request)
+    return redirect('home')
+
+def login_view(request):
+    context = {}
+    user=request.user
+    if user.is_authenticated:
+        return redirect('user')
+    if request.POST:
+        form=AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email=form.cleaned_data.get('email')
+            password=form.cleaned_data.get('password')
+            userl = authenticate(email=email, password=password)
+
+            if userl is not None:
+                login(request, userl)
+                Logs.objects.create(userS = request.user, action = Action.LOGIN)
+                return redirect('user')
+
+            else:
+                messages.error(request, "email and password don't match")
+
+    form = AccountAuthenticationForm()
+    context['login_form'] = form
+    return render(request, "myapp/login.html", context)
+
+def my_files(request):
+    context = {}
+    user=request.user
+    if user.is_authenticated:
+        if request.POST:
+            req=request.POST['action'].split("|")
+            file= FileMod.objects.filter(id=int(req[1])).first()
+            file_name= file.fileF.name
+            if req[0] =="DELETE":
+                FileMod.objects.filter(id=int(req[1])).delete()
+                os.remove(file_name)
+                Logs.objects.create(userS = request.user, action = Action.DELETE)
+            if req[0] == "DOWNLOAD":
+                f = open(file_name, 'rb')
+                response =FileResponse(f)    
+                response['Content-Disposition']='attachment;filename='+file.__str__()
+                Logs.objects.create(userS = request.user, action = Action.DOWNLOAD)
+                return response
+            if req[0] == "SHARE":
+
+                form=ShareForm(request.POST)
+                if form.is_valid():
+                    share=form.save(commit=False)
+                    share.fileS=file
+                    share.save()
+                    Logs.objects.create(userS = request.user, action = Action.SHARE)
+                
+        context['files']=FileMod.objects.all().filter(owner=user)
+        context['share']=ShareForm
+
+        return render(request, 'myapp/my_files.html', context)
+    else:
+        return redirect('home',request)
+
+    
+def public_files(request):
+    context = {}
+    user=request.user
+    if user.is_authenticated:
+        if request.POST:
+            req=request.POST['action'].split("|")
+            file= FileMod.objects.filter(id=int(req[1])).first()
+            if req[0] == "DOWNLOAD":                
+                file_name= file.fileF.name
+                f = open(file_name, 'rb')
+                response =FileResponse(f)    
+                response['Content-Disposition']='attachment;filename='+file.__str__()
+                Logs.objects.create(userS = request.user, action = Action.DOWNLOAD)
+                return response
+            if req[0] == "RATE":
+                form = OpinionForm(request.POST)
+                if form.is_valid():                
+                    obj = form.save(commit=False)
+                    opinion_exist = Opinion.objects.all().filter(fileS=file,userS=user)
+                    if opinion_exist == None:
+                        obj.fileS = file
+                        obj.userS = user
+                        obj.save()
+                        Logs.objects.create(userS = request.user, action = Action.RATE)
+        
+        context['files']=FileMod.objects.all().filter(is_public=True)
+        context["opinion"]=OpinionForm
+        return render(request, 'myapp/public_files.html', context)
+    else:
+        return redirect('home', request)
+
+
+def shared_files(request):
+    context = {}
+    user=request.user
+    if user.is_authenticated:
+        if request.POST:
+            req=request.POST['action'].split("|")
+            file= FileMod.objects.filter(id=int(req[1])).first()
+            if req[0] == "DOWNLOAD":                
+                file_name= file.fileF.name
+                f = open(file_name, 'rb')
+                response =FileResponse(f)    
+                response['Content-Disposition']='attachment;filename='+file.__str__()
+                Logs.objects.create(userS = request.user, action = Action.DOWNLOAD)
+                return response
+        
+        else:
+            pass
+        shared = SharedFile.objects.all().filter(userS = user)
+        context['files']=FileMod.objects.all().filter(id__in =shared)
+        return render(request, 'myapp/shared_files.html', context)
+    else:
+        return redirect('home', request)
+
+def public(request):
+    context = {}
+    if request.POST:
+            req=request.POST['action'].split("|")
+            file= FileMod.objects.filter(id=int(req[1])).first()
+            if req[0] == "DOWNLOAD":                
+                file_name= file.fileF.name
+                f = open(file_name, 'rb')
+                response =FileResponse(f)    
+                response['Content-Disposition']='attachment;filename='+file.__str__()
+                return response
+    else:
+        pass
+    context['files']=FileMod.objects.all().filter(is_public=True)
+    return render(request, 'myapp/public.html', context)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
